6ab904cb159253d672f264008e06a02657afd04e
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-11-26 13:07:29 -0600

    added Header and some basic styling to it.

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
 import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
+      <Header />
       <Nav />
-      <main className="mx-3">
+      <main className="contentContainer">
         <Outlet />
       </main>
       <Footer />
-      
     </>
   );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,7 +2,7 @@ import Content from "../components/Content";
 
 export default function Home() {
   return (
-    <Content title="Shanna Brooks">
+    <Content title="About Me">
       <div className="row align-items-center">
         <div className="col-4">
           <img style={{width: "100%"}} src="https://picsum.photos/640/360" alt="" />
