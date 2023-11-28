40219c4deb039cd6a1002b176b7b44a00cb535be
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-11-27 22:58:15 -0600

    added stock photos for portfolio images

--- README.md
+++ README.md
@@ -28,4 +28,19 @@ Application must be deployed to Netlify.
 
 hex triangles Photo by <a href="https://unsplash.com/@blankest?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Jack T</a> on <a href="https://unsplash.com/photos/a-close-up-of-a-ceiling-with-a-pattern-on-it-6P3AZeUEtfU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
 
+Cassserole Photo by <a href="https://unsplash.com/@sheri_silver?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">sheri silver</a> on <a href="https://unsplash.com/photos/white-ceramic-platter-q5C-_coAZR4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+
+Lightning Photo by <a href="https://unsplash.com/@noaa?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">NOAA</a> on <a href="https://unsplash.com/photos/lightning-during-nighttime-5YryiYcFvtA?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+Notebook Photo by <a href="https://unsplash.com/@m15ky?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Mike Tinnion</a> on <a href="https://unsplash.com/photos/black-and-silver-retractable-pen-on-blank-book-3ym6i13Y9LU?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Calendar Photo by <a href="https://unsplash.com/@esteejanssens?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Estée Janssens</a> on <a href="https://unsplash.com/photos/white-ceramic-mug-with-coffee-on-top-of-a-planner-aQfhbxailCs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+Nintendo Photo by <a href="https://unsplash.com/@ravipalwe?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Ravi Palwe</a> on <a href="https://unsplash.com/photos/white-and-black-nintendo-game-controller-_JTF0Prc7jc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+Lock Photo by <a href="https://unsplash.com/@towfiqu999999?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Towfiqu barbhuiya</a> on <a href="https://unsplash.com/photos/a-golden-padlock-sitting-on-top-of-a-keyboard-FnA5pAzqhMM?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+
+
+  
   
--- src/Pages/Portfolio.jsx
+++ src/Pages/Portfolio.jsx
@@ -3,42 +3,42 @@ import Content from "../components/Content";
 export default function Portfolio() {
   const projects = [
     {
-        "image_url": "src/images/recipeblog.png",
+        "image_url": "src/images/sheri-silver-q5C-_coAZR4-unsplash.jpg",
         "title": "Recipe Blog",
         "repo_link": "https://github.com/Shanna-not-Shawna/cms-blog",
         "deployed_link": "https://safe-beyond-36437-fd4baa9db3a8.herokuapp.com/",
         "description": "Recipe Blog technologies used"
     },
     {
-        "image_url": "src/images/weatherdash.png",
+        "image_url": "src/images/noaa-5YryiYcFvtA-unsplash.jpg",
         "title": "Weather Dashboard",
         "repo_link": "https://github.com/Shanna-not-Shawna/weather-dashboard",
         "deployed_link": "https://shanna-not-shawna.github.io/weather-dashboard/",
         "description": "Weather Dashboard technologies used"
     },
     {
-        "image_url": "src/images/notetaker.png",
+        "image_url": "src/images/mike-tinnion-3ym6i13Y9LU-unsplash.jpg",
         "title": "Note Taker",
         "repo_link": "https://github.com/Shanna-not-Shawna/note-taker",
         "deployed_link": "https://mysterious-island-53745-682eedebbb58.herokuapp.com/",
         "description": "Note Taker technologies used"
     },
     {
-        "image_url": "src/images/workdayscheduler.png",
+        "image_url": "src/images/estee-janssens-aQfhbxailCs-unsplash.jpg",
         "title": "Work Day Scheduler",
         "repo_link": "https://github.com/Shanna-not-Shawna/work-day-scheduler",
         "deployed_link": "https://shanna-not-shawna.github.io/work-day-scheduler/",
         "description": "Work Day Scheduler technologies used"
     },
     {
-        "image_url": "src/images/nintendoquiz.png",
+        "image_url": "src/images/ravi-palwe-_JTF0Prc7jc-unsplash.jpg",
         "title": "Nintendo Quiz",
         "repo_link": "https://github.com/Shanna-not-Shawna/nintendo-quiz",
         "deployed_link": "https://shanna-not-shawna.github.io/nintendo-quiz/",
         "description": "Nintendo Quiz technologies used"
     },
     {
-        "image_url": "src/images/passwordgen.png",
+        "image_url": "src/images/towfiqu-barbhuiya-FnA5pAzqhMM-unsplash.jpg",
         "title": "Password Generator",
         "repo_link": "https://github.com/Shanna-not-Shawna/password-generator",
         "deployed_link": "https://shanna-not-shawna.github.io/password-generator/",
@@ -58,7 +58,9 @@ export default function Portfolio() {
                   <img src={item.image_url} className="card-img-top" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title">{item.title}</h5>
-                    <a href={item.repo_link}><i className="pi pi-github" style={{ fontSize: '1rem', color: 'black' }}></i></a>
+                    <p>{item.description}</p>
+                    <a href={item.repo_link}><i className="pi pi-github" style={{ fontSize: '2rem', color: 'black' }}></i></a>
+                    <a href={item.deployed_link}><i className="pi pi-link" style={{ fontSize: '2rem', color: 'black' }}></i></a>
                   </div>
                 </div>
               </div>
