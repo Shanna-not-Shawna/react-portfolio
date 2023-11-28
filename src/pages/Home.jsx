943ff0e0b4eac884ed4008fde7a59c9e28ae656c
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-11-28 09:36:37 -0600

    working on deploy error

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import Content from "../components/Content";
+
+export default function Home() {
+  return (
+    <Content title="About Me">
+      <div className="row align-items-center">
+        <div className="col-4">
+          <img style={{width: "100%"}} src="src\images\profilepic.jpg" alt="Shanna Brooks" />
+        </div>
+        <div className="col-8">
+          <h2>About Me</h2>
+          <p> Hi there! I'm Shanna Brooks, a passionate problem solver with a knack for coding based in Texas. Recently graduating from a coding bootcamp, I'm on a mission to dive into the world of full-stack development.</p>
+          <h5>What Drives Me:</h5>
+          <p>I thrive on turning complex challenges into elegant solutions. From my roots in real estate title and customer service to custom apparel decoration, I bring a diverse set of skills to the tech table. Every obstacle is a new puzzle to solve, and I'm ready to tackle them head-on.</p>
+          <h5>Beyond the Screen:</h5>
+          <p>When I'm not immersed in code, you'll find me building intricate LEGO masterpieces, piecing together challenging jigsaws, or immersing myself in the realm of video games. It's this same love for creativity and problem-solving that fuels my passion for coding.</p>
+          <h5>Let's Connect:</h5>
+          <p>I'm excited to bring my unique background and skills to the tech world. Let's connect and explore the possibilities together!</p>
+        </div>
+      </div>
+    </Content>
+
+  );
+}
---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,74 @@
+import Content from "../components/Content";
+
+export default function Portfolio() {
+  const projects = [
+    {
+        "image_url": "src/images/sheri-silver-q5C-_coAZR4-unsplash.jpg",
+        "title": "Recipe Blog",
+        "repo_link": "https://github.com/Shanna-not-Shawna/cms-blog",
+        "deployed_link": "https://safe-beyond-36437-fd4baa9db3a8.herokuapp.com/",
+        "description": "Node.js, Inquirer, MySQL"
+    },
+    {
+        "image_url": "src/images/noaa-5YryiYcFvtA-unsplash.jpg",
+        "title": "Weather Dashboard",
+        "repo_link": "https://github.com/Shanna-not-Shawna/weather-dashboard",
+        "deployed_link": "https://shanna-not-shawna.github.io/weather-dashboard/",
+        "description": "HTML, CSS, Open Weather API"
+    },
+    {
+        "image_url": "src/images/mike-tinnion-3ym6i13Y9LU-unsplash.jpg",
+        "title": "Note Taker",
+        "repo_link": "https://github.com/Shanna-not-Shawna/note-taker",
+        "deployed_link": "https://mysterious-island-53745-682eedebbb58.herokuapp.com/",
+        "description": "Express.js"
+    },
+    {
+        "image_url": "src/images/estee-janssens-aQfhbxailCs-unsplash.jpg",
+        "title": "Work Day Scheduler",
+        "repo_link": "https://github.com/Shanna-not-Shawna/work-day-scheduler",
+        "deployed_link": "https://shanna-not-shawna.github.io/work-day-scheduler/",
+        "description": "HTML, CSS, jQuery"
+    },
+    {
+        "image_url": "src/images/ravi-palwe-_JTF0Prc7jc-unsplash.jpg",
+        "title": "Nintendo Quiz",
+        "repo_link": "https://github.com/Shanna-not-Shawna/nintendo-quiz",
+        "deployed_link": "https://shanna-not-shawna.github.io/nintendo-quiz/",
+        "description": "HTML, CSS, JavaScript"
+    },
+    {
+        "image_url": "src/images/towfiqu-barbhuiya-FnA5pAzqhMM-unsplash.jpg",
+        "title": "Password Generator",
+        "repo_link": "https://github.com/Shanna-not-Shawna/password-generator",
+        "deployed_link": "https://shanna-not-shawna.github.io/password-generator/",
+        "description": "JavaScript"
+    }
+]
+
+  return (
+    <Content title="Portfolio">
+      <div className="row row-cols-2 g-4">
+        {
+          projects.map((item) => {
+            return (
+              <div key={item.title} className="col">
+                <div className="card">
+                  <img src={item.image_url} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p>{item.description}</p>
+                    <a href={item.repo_link}><i className="pi pi-github" style={{ fontSize: '2rem', color: 'black' }}></i></a>
+                    <a href={item.deployed_link}><i className="pi pi-link" style={{ fontSize: '2rem', color: 'black' }}></i></a>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+
+      </div>
+
+    </Content>
+  );
+}
